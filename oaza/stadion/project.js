453c327237a94b059999848d0bd466223342209d
bajerskim11-eng <bajerskim11@gmail.com>
2026-08-30 12:56:09 +0200

    Rozbuduj plan Stadionu o Festiwal Śląski, partnerów i program

--- oaza/stadion/project.js
+++ oaza/stadion/project.js
@@ -1,31 +1,40 @@
 const zones = [
- ['01','RELAX','Zieleń + siedziska','Cień, ławki, leżaki, roślinność i miejsce na zwykłe spędzanie czasu.'],
- ['02','MUZYKA','Mała scena','Koncerty lokalnych wykonawców, jamy, DJ-e, kino i wydarzenia.'],
- ['03','SPORT','Aktywność','Proste strefy ruchu i rekreacji dostępne bez formalności.'],
- ['04','SPOTKANIA','Stół OAZY','Miejsce do siedzenia, rozmów i organizowania własnych inicjatyw.'],
- ['05','KULTURA','Beboki','Śląskie historie, sztuka i przyszła warstwa AR jako część miejsca.'],
- ['06','MŁODZI','Strefa twórców','Przestrzeń dla ludzi, którzy chcą coś zrobić, pokazać albo zorganizować.']
+ ['01','RELAX','Leżaki + zieleń','Prosta strefa odpoczynku: leżaki, stoły, cień, roślinność i miejsce na zwykłe spotkania.'],
+ ['02','GASTRO','Food trucki','Lokalne jedzenie, kawa i napoje. Partnerzy dostają miejsce, publiczność dostaje wybór.'],
+ ['03','KULTURA','Galeria w pawilonach','Producenci pawilonów i modułów udostępniają swoje realizacje, a my wypełniamy je sztuką lokalnych twórców.'],
+ ['04','MUZYKA','Scena lokalna','Koncerty młodych kapel, wokalistów, DJ-ów i lokalnych wykonawców. Bez bariery wejścia dla publiczności.'],
+ ['05','FILM','Kino pod ruinami','Wieczorne pokazy niszowych filmów, krótkich metraży, dokumentów, teledysków i produkcji lokalnych.'],
+ ['06','AWF','Miasteczko studenckie','Sport, integracja i aktywności przygotowywane wspólnie ze studentami AWF.'],
+ ['07','SPOTKANIA','Namiot całoroczny','Prosta, ogrzewana zimą przestrzeń do spotkań, warsztatów i wydarzeń.'],
+ ['08','BEBOKI','Śląski charakter','Beboki jako przewodnicy projektu i warstwa opowieści, która łączy kulturę miejsca z OAZĄ.']
 ];
 const phases = [
- ['A','POZNANIE TERENU','Lokalizacja, własność, dostęp, stan infrastruktury, ograniczenia i potrzeby.'],
- ['B','PROJEKT SPOTU','Układ stref, pierwsza infrastruktura, bezpieczeństwo i sposób użytkowania.'],
- ['C','PARTNERZY','Firmy i osoby obejmujące konkretne elementy realizacji.'],
- ['D','PIERWSZE URUCHOMIENIE','Minimalna wersja miejsca, pierwsze wydarzenie i sprawdzenie działania.']
+ ['A','ZGODA I TEREN','Ustalamy z AWF zakres udostępnienia, termin, zasady bezpieczeństwa, dostęp do mediów i dokładne miejsce wydarzenia.'],
+ ['B','KONCEPCJA 30 DNI','Wspólnie przygotowujemy prosty plan: scena, pawilony, namiot, food trucki, leżaki, wystawy, kino i aktywności AWF.'],
+ ['C','PARTNERZY','Zamiast kupować wszystko, zbieramy partnerów rzeczowych i sponsorów. Każdy może objąć konkretny element projektu.'],
+ ['D','NABÓR LOKALNY','Otwieramy nabór dla muzyków, malarzy, fotografów, rzeźbiarzy, filmowców, food trucków i studentów.'],
+ ['E','MONTAŻ','W ostatnich dniach składamy teren: pawilony, namiot, scena, światło, leżaki, wystawy i oznaczenia.'],
+ ['F','FESTIWAL ŚLĄSKI','Otwieramy miejsce dla ludzi. Pierwsza edycja jest testem: sprawdzamy, co działa i co warto zostawić na stałe.']
 ];
 const slots = [
- ['ARCHITEKT / PROJEKTANT','Koncepcja przestrzeni SPOTU.'],
- ['PARTNER INFRASTRUKTURY','Siedziska, zadaszenia, moduły i wyposażenie.'],
- ['PARTNER ŚWIATŁA','Oświetlenie i bezpieczne korzystanie po zmroku.'],
- ['PARTNER WYDARZEŃ','Pierwsze koncerty i wydarzenia lokalne.'],
- ['TWÓRCA / ARTYSTA','Sztuka, identyfikacja i elementy kultury.'],
- ['PARTNER ZIELENI','Projekt i wykonanie zielonej części SPOTU.'],
- ['TECHNOLOGIA','QR, mapa i przyszła warstwa AR.'],
- ['GŁÓWNY PARTNER SPOTU','Wsparcie powstania całego miejsca.']
+ ['PRODUCENT DOMÓW MODUŁOWYCH','Jeden moduł do pokazania na żywo. Może służyć jako punkt informacji, warsztat albo przestrzeń partnera.'],
+ ['PRODUCENT PAWILONÓW','Pawilon wystawowy do czasowego ustawienia na terenie. W środku pokazujemy prace lokalnych artystów.'],
+ ['PARTNER NAMIOTU','Namiot eventowy, najlepiej z możliwością ogrzewania i wykorzystania również jesienią oraz zimą.'],
+ ['PARTNER LEŻAKÓW','Leżaki, stoły i proste wyposażenie strefy chill.'],
+ ['PARTNER SCENY','Mała scena / podest dla lokalnych koncertów i wydarzeń.'],
+ ['PARTNER DŹWIĘKU I ŚWIATŁA','Nagłośnienie, podstawowe oświetlenie i obsługa techniczna.'],
+ ['FOOD TRUCKI I LOKALNA GASTRONOMIA','Zapraszamy lokalnych przedsiębiorców do stworzenia strefy gastro.'],
+ ['ARTYŚCI I GALERIE','Malarze, fotografowie, rzeźbiarze, ilustratorzy i twórcy lokalni.'],
+ ['FILMOWCY','Lokalne i niszowe produkcje na wieczorne pokazy.'],
+ ['PARTNER SPORTOWY','Sprzęt i aktywności przygotowywane razem ze studentami AWF.'],
+ ['PARTNER ZIELENI','Rośliny, donice, ziemia i proste elementy zielonej przestrzeni.'],
+ ['SPONSOR GŁÓWNY','Finansowe wsparcie uruchomienia Festiwalu Śląskiego.'],
+ ['WOLONTARIUSZE','Ludzie, którzy chcą pomóc przed wydarzeniem, w trakcie i przy demontażu.']
 ];
 
 document.querySelector('#zones').innerHTML = zones.map(z=>`<article class="zone"><span>${z[0]} / ${z[1]}</span><h3>${z[2]}</h3><p>${z[3]}</p></article>`).join('');
 document.querySelector('#timeline').innerHTML = phases.map(p=>`<article class="phase"><b>${p[0]}</b><div><strong>${p[1]}</strong><p>${p[2]}</p></div></article>`).join('');
-document.querySelector('#slots').innerHTML = slots.map(s=>`<article class="slot"><span>OTWARTY SLOT</span><h3>${s[0]}</h3><p>${s[1]}</p><a href="/oaza/">ZGŁOŚ SIĘ →</a></article>`).join('');
+document.querySelector('#slots').innerHTML = slots.map(s=>`<article class="slot"><span>OTWARTY SLOT</span><h3>${s[0]}</h3><p>${s[1]}</p><a href="/oaza/">DOŁĄCZ →</a></article>`).join('');
 
 document.querySelectorAll('.image-slot').forEach(slot=>{
   const src=slot.dataset.image;
